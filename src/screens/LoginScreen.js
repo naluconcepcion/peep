6714c30772e965de8d21467cfd614bb11913eb6f
Shihao Cao <shehowcow@gmail.com>
2021-01-09 22:48:59 -0500

    pre mod home screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Title } from 'react-native-paper';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 
+import { AuthContext } from '../navigation/AuthProvider';
+
 export default function LoginScreen({ navigation }) {
+    const { login } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
@@ -27,6 +30,7 @@ export default function LoginScreen({ navigation }) {
           title='Login'
           modeValue='contained'
           labelStyle={styles.loginButtonLabel}
+          onPress={() => login(email, password)}
         />
         <FormButton
           title='New user? Join here'
