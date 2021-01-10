91b791fa741a81d350d7aeb4923f251bafd2baa9
Shihao Cao <shehowcow@gmail.com>
2021-01-10 00:07:26 -0500

    yeetus

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -2,12 +2,35 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 
-const Stack = createStackNavigator();
+import AddRoomScreen from '../screens/AddRoomScreen';
+
+// create two new instances
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+function ChatApp() {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee'
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22
+        }
+      }}
+    >
+      <ChatAppStack.Screen name='Home' component={HomeScreen} />
+    </ChatAppStack.Navigator>
+  );
+}
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+    <ModalStack.Navigator mode='modal' headerMode='none'>
+      <ModalStack.Screen name='ChatApp' component={ChatApp} />
+      <ModalStack.Screen name='AddRoom' component={AddRoomScreen} />
+    </ModalStack.Navigator>
   );
 }
