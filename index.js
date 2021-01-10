fd8f6be2a34f3bdb2ff4e5abac09e0049eaf974a
Shihao Cao <shehowcow@gmail.com>
2021-01-09 20:51:52 -0500

    it works

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+
+// import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Routes from './src/navigation/Routes';
+import { name as appName } from './app.json';
 
 /**
  * Wrap all providers here
@@ -13,3 +17,5 @@ export default function Providers() {
     </PaperProvider>
   );
 }
+
+AppRegistry.registerComponent(appName, () => Providers);
